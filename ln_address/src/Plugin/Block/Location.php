44a96b15425dc55eec197fe7171b57d7b76e7477
Amarlata Kumari <amarlata.kumari@sapient.com>
2021-02-01 17:01:03 +0530

    add address module.

--- ln_address/src/Plugin/Block/Location.php
+++ ln_address/src/Plugin/Block/Location.php
@@ -4,6 +4,11 @@
 
   use Drupal;
   use Drupal\Core\Block\BlockBase;
+  use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+  use Drupal\ln_address\Service\LnAddressService;
+  use Symfony\Component\DependencyInjection\ContainerInterface;
+  use Drupal\Core\Config\ConfigFactoryInterface;
+  use Drupal\Core\Cache\Cache;
 
   /**
    * Provides a 'Location' block.
@@ -15,14 +20,57 @@
    *   module = "ln_address_location",
    * )
    */
-  class Location extends BlockBase {
+  class Location extends BlockBase implements ContainerFactoryPluginInterface {
+
+    /**
+     * @var LnAddressService $configAddress
+     */
+    protected $configAddress;
+
+    /**
+     * @var ConfigFactoryInterface $configFactory
+     */
+    protected $configFactory ;
+
+    /**
+     * Location constructor.
+     *
+     * @param array $configuration
+     * @param $plugin_id
+     * @param $plugin_definition
+     * @param \Drupal\ln_address\Service\LnAddressService $configAddress
+     * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
+     */
+    public function __construct(array $configuration, $plugin_id, $plugin_definition, LnAddressService $configAddress, ConfigFactoryInterface $configFactory) {
+      parent::__construct($configuration, $plugin_id, $plugin_definition);
+      $this->configAddress = $configAddress;
+      $this->configFactory = $configFactory;
+    }
+
+    /**
+     * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+     * @param array $configuration
+     * @param string $plugin_id
+     * @param mixed $plugin_definition
+     *
+     * @return static
+     */
+    public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+      return new static(
+        $configuration,
+        $plugin_id,
+        $plugin_definition,
+        $container->get('ln_address'),
+        $container->get('config.factory')
+      );
+    }
 
     /**
      * {@inheritdoc}
      */
     public function build() {
-      $date = \Drupal::service('ln_address')->generateDataTimeByTimezone();
-      $config = \Drupal::config('ln_address.settings');
+      $date = $this->configAddress->generateDataTimeByTimezone();
+      $config = $this->configFactory->get('ln_address.settings');
       $data = (!empty($config) && is_object($config) && !empty($config->get('country'))) ? true : false;
       $site_address_details = [
         '#theme'             => 'site_address_block',
@@ -31,8 +79,14 @@
         '#timezone'          => $config->get('timezone'),
         '#time'              => $date,
         '#is_data'           => $data
-
       ];
       return $site_address_details;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCacheMaxAge() {
+      return 0;
     }
   }
